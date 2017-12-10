176d5d6e2ab9eca6b575aae6cf1f327e8693a1eb
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-10 15:59:51 +0100

    correction

---
+++ divide.js
@@ -0,0 +1,12 @@
+var a = parseInt(process.argv[2]);
+var b = parseInt(process.argv[3]);
+
+function divide (a ,b) {
+	if (a ,b === 0){
+	return (0);
+	} else {
+	return (a /b);
+	}
+}
+
+console.log(divide(a ,b))
